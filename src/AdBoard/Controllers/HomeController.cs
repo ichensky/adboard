4afea6115821763e133e2969659ce9657644d791
Ivan Chensky <ichensky@live.com>
2021-01-28 01:05:43 -0800

    add fix

---
+++ src/AdBoard/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AdBoard.Models;
+using Application.Users.GetUserName;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMediator _mediatr;
+
+        public HomeController(ILogger<HomeController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediatr = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await _mediatr.Send(new GetUserNameQuery(Guid.NewGuid()));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
